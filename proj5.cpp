8e4a9de92a3f9ca887a0dca1099ab65d405b061d
Ben Zech <72528903+TechZech@users.noreply.github.com>
2021-01-09 23:02:12 -0500

    Update proj5.cpp

--- proj5.cpp
+++ proj5.cpp
@@ -1,6 +1,6 @@
 /*		PROJ5.CPP
-	Ben Zech	Assignment 5
-	Mr. Myers	Due Monday, November 23rd 11:59pm
+	Ben Zech	11/23/2020
+	All work was written by Benjamin Zech. *Do not steal or plagerize*
 */
 
 #include <iostream>
